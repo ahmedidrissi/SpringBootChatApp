8fdaf779b8cfa6ea7966dce9870e10023378596a
ahmedidrissi <idrissiahmed2002@gmail.com>
2023-07-14 20:06:44 +0100

    first commit

---
+++ src/main/java/com/example/chat/config/WebSocketEventListener.java
@@ -0,0 +1,32 @@
+package com.example.chat.config;
+
+
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import com.example.chat.chat.ChatMessage;
+import com.example.chat.chat.MessageType;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class WebSocketEventListener {
+
+    @EventListener
+    public void handleWebSocketConnectListener(SessionDisconnectEvent event) {
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        String username = (String) headerAccessor.getSessionAttributes().get("username");
+        if (username != null) {
+            log.info("User Disconnected : " + username);
+            var chatMessage = new ChatMessage.builder()
+            .type(MessageType.LEAVE)
+            .sender(username)
+            .build();
+        }
+    }
+}
