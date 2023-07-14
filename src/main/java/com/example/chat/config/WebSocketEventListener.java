0088f0aac98da8c533f1438068299a238824ea93
ahmedidrissi <idrissiahmed2002@gmail.com>
2023-07-14 20:15:02 +0100

    update

--- src/main/java/com/example/chat/config/WebSocketEventListener.java
+++ src/main/java/com/example/chat/config/WebSocketEventListener.java
@@ -2,6 +2,7 @@ package com.example.chat.config;
 
 
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
@@ -17,16 +18,18 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class WebSocketEventListener {
 
+    private final SimpMessageSendingOperations messageTemplate;
+
     @EventListener
     public void handleWebSocketConnectListener(SessionDisconnectEvent event) {
         StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
         String username = (String) headerAccessor.getSessionAttributes().get("username");
         if (username != null) {
             log.info("User Disconnected : " + username);
-            var chatMessage = new ChatMessage.builder()
-            .type(MessageType.LEAVE)
-            .sender(username)
-            .build();
+            var chatMessage = new ChatMessage();
+            chatMessage.setType(MessageType.LEAVE);
+            chatMessage.setSender(username);
+            messageTemplate.convertAndSend("/topic/public", chatMessage);
         }
     }
 }
